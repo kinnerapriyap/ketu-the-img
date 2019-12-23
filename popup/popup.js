b9d4f7828618c7d1bd3e6958265765996845652d
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-23 14:23:22 +0900

    Move content script to separate fiel

--- popup/popup.js
+++ popup/popup.js
@@ -8,7 +8,7 @@ changeColor.onclick = function(element) {
   let color = element.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";'
+      file: "contentScript.js"
     });
   });
 };
