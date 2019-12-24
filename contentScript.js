18efc417a09d30bb28e11d09cd7853c06fdae0ed
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-24 15:23:37 +0900

    Replace with img src

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,6 @@
 var textAreas = document.querySelectorAll("textArea[name*='[body]']")
 textAreas.forEach(function(textArea) {
-    textArea.style.background = 'red';
+    var defaultPatt = /\!\[\w+\]\(\w+\)/gm
+    var newPatt = '<img src="$2" width=300/>'
+    textArea.value = textArea.value.replace(defaultPatt, newPatt)
 })
