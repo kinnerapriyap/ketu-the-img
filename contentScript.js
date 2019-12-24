3ecdc52994e3b99797e367c2cdce884a050c582d
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-24 17:31:53 +0900

    Fix pattern matching

--- contentScript.js
+++ contentScript.js
@@ -4,7 +4,7 @@ chrome.storage.sync.get(['width'], function(result) {
   imageWidth = result.width
   console.log("Get width input", "width", result.width);
   textAreas.forEach(function(textArea) {
-    var defaultPatt = /\!\[(\w+)\]\((\w+)\)/gm;
+    var defaultPatt = /\!\[(.*?)\]\((.*?)\)/gm;
     var newPatt = '<img src="$2" width=' + imageWidth + ' />';
     textArea.value = textArea.value.replace(defaultPatt, newPatt);
   });
