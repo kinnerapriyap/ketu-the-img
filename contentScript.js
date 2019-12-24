4a0ac7d4209e4dd249999dd2b8de28d018ec9133
kinnerapriyap <kinnerapriyap@gmail.com>
2019-12-24 16:29:56 +0900

    Format content script

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,6 @@
-var textAreas = document.querySelectorAll("textArea[name*='[body]']")
+var textAreas = document.querySelectorAll("textArea[name*='[body]']");
 textAreas.forEach(function(textArea) {
-    var defaultPatt = /\!\[\w+\]\(\w+\)/gm
-    var newPatt = '<img src="$2" width=300/>'
-    textArea.value = textArea.value.replace(defaultPatt, newPatt)
-})
+  var defaultPatt = /\!\[\w+\]\(\w+\)/gm;
+  var newPatt = '<img src="$2" width=300/>';
+  textArea.value = textArea.value.replace(defaultPatt, newPatt);
+});
