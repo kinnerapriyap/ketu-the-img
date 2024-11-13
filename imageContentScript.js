1f69ad71f5e1d078b663b439bea8ff76eb222558
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-11-13 09:15:23 +0100

    Fix js things

--- imageContentScript.js
+++ imageContentScript.js
@@ -1,11 +1,11 @@
-var textAreas = document.querySelectorAll("textArea[name*='[body]']");
-var imageWidth = 300
+const textAreas = document.querySelectorAll("textArea[name*='[body]']");
+let imageWidth = 300;
 chrome.storage.sync.get(['width'], function(result) {
   imageWidth = result.width
   console.log("Get width input", result.width);
   textAreas.forEach(function(textArea) {
-    var defaultPatt = /\!\[(.*?)\]\((.*?)\)/gm;
-    var newPatt = '<img src="$2" width=' + imageWidth + ' />';
+    const defaultPatt = /!\[(.*?)]\((.*?)\)/gm;
+    const newPatt = '<img alt="" src="$2" width=' + imageWidth + ' />';
     textArea.value = textArea.value.replace(defaultPatt, newPatt);
   });
 });
