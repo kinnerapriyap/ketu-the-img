3298cb0fd410e21964953eb10f42f1384b94842e
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2025-01-13 06:24:24 +0100

    Allow change in all text areas

--- imageContentScript.js
+++ imageContentScript.js
@@ -1,5 +1,5 @@
-const textAreas = document.querySelectorAll("textArea");
 chrome.storage.sync.get(['width', 'alt'], function (result) {
+  const textAreas = document.querySelectorAll("textArea, textarea");
   textAreas.forEach(function (textArea) {
     const defaultPatt = /!\[(.*?)]\((.*?)\)/gm;
     const newPatt = '<img alt="' + result.alt + '" src="$2" width=' + result.width + ' />';
